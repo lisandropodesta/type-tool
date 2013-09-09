31be3029ce1a3d4c28bb5975223f079ac3660245
Lisandro Podestá <lisandro.podesta@gmail.com>
2013-09-09 16:04:22 -0300

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-type-tool
-=========
+is-type
+=======
 
-Flexible type checking
+Flexible type checking including isUndefined, isNull, isBoolean, isNumber, isDate, isString, isRegExp, isArray, isArguments, isFunction and isObject.
+
+## Usage
+
+### isUndefined, isNull, isBoolean, isNumber, isDate, isString, isRegExp, isArray, isArguments, isFunction and isObject
+Determines whether a value is of a specific type.
+
+```javascript
+var type = require( "type-tool" );
+if ( type.isFunction( value ) ) {
+  // value is a function
+}
+```
+
+### type.name( value )
+
+Returns type name of value. Possible results are: "undefined", "null", "boolean", "number", "date", "string", "regexp", "array", "arguments", "function" and "object"
+
+```javascript
+var type = require( "type-tool" );
+if ( type.name( value ) == "array" ) {
+  // value is an array
+}
+```
+
+### type.get( value )
+
+Returns type information of value including name and specific type flags.
+
+```javascript
+var type = require( "type-tool" );
+var ti = type.get( [] ); // -> ti == { name: "array", isArray: true }
+
+ti = type.get( value );
+if ( ti.isArray ) {
+  // value is an array
+}
+else if ( ti.isFunction ) {
+  // value is a function
+}
+else if ( ti.isNumber ) {
+  // value is a number
+}
+```
+
+## License
+
+type-tool is available under the [MIT license] (http://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,99 @@
+//
+// Exports
+//
+module.exports = Type.prototype;
+
+//
+// toString
+//
+var toString = Object.prototype.toString;
+
+//
+// Types mapping
+//
+var TYPES = {
+  "[object Undefined]": "Undefined",
+  "[object Null]": "Null",
+  "[object Boolean]": "Boolean",
+  "[object Number]": "Number",
+  "[object Date]": "Date",
+  "[object String]": "String",
+  "[object RegExp]": "RegExp",
+  "[object Array]": "Array",
+  "[object Arguments]": "Arguments",
+  "[object Function]": "Function",
+  "[object Object]": "Object"
+};
+
+//
+// Create individual type testing functions
+//
+(function () {
+
+  function make_fn( n ) {
+    var
+      fun_name = "is" + TYPES[ n ],
+      type_name = TYPES[ n ].toLowerCase();
+
+    Type.prototype[ fun_name ] = function ( v ) {
+      return type_name === Type.prototype.name( v );
+    }
+  }
+
+  for ( var n in TYPES ) {
+    make_fn( n );
+  }
+})();
+
+//
+// Constructor
+//
+function Type() {
+}
+
+//
+// Gets capitalized type name
+//
+function getTypeName( v ) {
+  var
+    name;
+
+  if ( null === v ) {
+    return "Null";
+  }
+
+  if ( undefined === v ) {
+    return "Undefined";
+  }
+
+  name = TYPES[ toString.call( v ) ];
+
+  if ( !name )
+  {
+    name = typeof v;
+    name = name[ 0 ].toUpperCase() + name.slice( 1 );
+  }
+
+  return name;
+}
+
+//
+// Get type information
+//
+Type.prototype.get = function ( v ) {
+  var
+    name,
+    ret = {};
+
+  name = getTypeName( v );
+  ret.name = name.toLowerCase();
+  ret[ "is" + name ] = true;
+  return ret;
+}
+
+//
+// Get type as string
+//
+Type.prototype.name = function( v ) {
+  return getTypeName( v ).toLowerCase();
+}
